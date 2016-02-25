e0b583d2bdaa0931e7c617852f9f4d5908d500c7
Bloodham <bloodham25@gmail.com>
2016-02-25 13:52:17 -0500

    Added partials and pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Landing page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Landing Page' });
+});
+
+/* GET Landing page. */
+router.get('/people', function(req, res, next) {
+  res.render('people', { title: 'People Page' });
+});
+
+/* GET Landing page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Page' });
 });
 
 module.exports = router;
